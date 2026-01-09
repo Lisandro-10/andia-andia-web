8f0b91d86538d7896648f8f2394f4cc0bdf2c291
Lisandro <lisandroandia14@gmail.com>
2026-01-09 10:29:29 -0300

    initial config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'andia-web-assets.s3.sa-east-1.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+};
 
 export default nextConfig;
