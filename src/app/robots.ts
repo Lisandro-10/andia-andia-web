45b45f85e5919becc9f2882b12759fd3c8854213
Lisandro <lisandroandia14@gmail.com>
2026-07-20 13:52:07 -0300

    Se modifica robots para que no interfiera en la indexacion de paginas en google

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: ['/api/'],
       },
     ],
     sitemap: 'https://estudioandia.com/sitemap.xml',
